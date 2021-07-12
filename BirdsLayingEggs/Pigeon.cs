4e038eba67fb2d81428898bb82e5213e1fbcb8ca
Tunji Oyewole <t.oyewole23@outlook.com>
2021-07-12 16:55:20 -0400

    Completed code

---
+++ BirdsLayingEggs/Pigeon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdsLayingEggs
+{
+    class Pigeon : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] eggs = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+            }
+            return eggs;
+        }
+    }
+}
