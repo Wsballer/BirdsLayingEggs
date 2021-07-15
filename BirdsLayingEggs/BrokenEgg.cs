37b75356fcf64e41a015de8f6ce36b34dbdffdca
Tunji Oyewole <t.oyewole23@outlook.com>
2021-07-15 17:00:00 -0400

    Added subclass BrokenEgg

---
+++ BirdsLayingEggs/BrokenEgg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdsLayingEggs
+{
+    class BrokenEgg : Egg
+    {
+        public BrokenEgg(double size, string color) : base(size, color)
+        {
+            Console.WriteLine("A bird laid a broken egg");
+        }
+    }
+}
